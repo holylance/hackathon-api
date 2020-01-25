dea363c260c2f6c3812e742249d5840492a4de40
holylance <holylance82@gmail.com>
2020-01-25 21:55:57 +0100

    add flyway and json structure

--- src/main/kotlin/com/example/demo/controller/HelloController.kt
+++ src/main/kotlin/com/example/demo/controller/HelloController.kt
@@ -1,12 +1,15 @@
 package com.example.demo.controller
 
+import com.example.demo.model.TestDTO
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
     @GetMapping("/helloworld")
-    fun greeting(): String {
-        return "Hello World"
+    @ResponseBody
+    fun greeting(): TestDTO {
+        return TestDTO(1.toBigInteger(), "name")
     }
 }
